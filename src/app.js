e223b8f39ed4f746f4c8561f7f82062491421800
Kaiena <kaienafmartins@gmail.com>
2023-04-16 09:52:10 -0300

    create post messages route

--- src/app.js
+++ src/app.js
@@ -68,4 +68,39 @@ app.get("/participants", async (req, res) => {
   }
 });
 
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const from = req.headers.user;
+
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required(),
+  });
+
+  const validation = messageSchema.validate(req.body, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  try {
+    const participant = await db
+      .collection("participants")
+      .findOne({ name: from });
+
+    if (!participant) {
+      res.status(422).send("Usuário não cadastrado");
+    }
+
+    await db
+      .collection("messages")
+      .insertOne({ from, to, text, type, time: dayjs().format("HH:mm:ss") });
+    res.status(201).send();
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
